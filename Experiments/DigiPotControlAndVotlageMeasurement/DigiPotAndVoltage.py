95736b0faa5864effe34b557b90a321454c3cd67
Ahmed-Waseem77 <ahmedwaseemr@aucegypt.edu>
2024-03-18 00:55:37 +0200

    code and documentation (needs schematic)

--- Experiments/DigiPotControlAndVotlageMeasurement/DigiPotAndVoltage.py
+++ Experiments/DigiPotControlAndVotlageMeasurement/DigiPotAndVoltage.py
@@ -1,6 +1,10 @@
+# Measure Voltage and control the digital pot accordingly
+# gradually increases voltage, once above a threshold it stops wipercontrol completely
+
 from machine import Pin
 import time
 import rp2
+
 
 # CS to INC setup
 def tci():
@@ -31,6 +35,7 @@ tdi()
 inc = Pin(12,mode=Pin.OUT,value=1)
 tid()
 
+ADCpin0 = machine.ADC(0)
 
 # This yields a pulse of 35 us which is counter intuitive
 # for the time being this suits our use case
@@ -38,14 +43,21 @@ tid()
 def blink(pin):
     pin.toggle()
     tihl()
+    return updateAndPrintVoltageMeasurement(ADCpin0)
     
 def wiperControl(increments,updownSelect):
     ud.value(updownSelect)
     tdi()
     for i in range(0,increments):
-        time.sleep_ms(2)
-        blink(inc)
+        time.sleep_ms(100)
+        value = blink(inc)
+        if(value > 1.7):
+            break
         
+def updateAndPrintVoltageMeasurement(ADCpin):
+    value = ADCpin.read_u16()
+    print(value/65535*3.3)
+    return value
         
 #10k ohms and 101ohm step
 #100 increments to max/min
@@ -53,6 +65,9 @@ def wiperControl(increments,updownSelect):
 while True:
     wiperControl(110,1)
     wiperControl(110,0)
+
+
+
 
 
 
---
+++ Experiments/DigiPotControlAndVotlageMeasurement/README.md
@@ -0,0 +1,9 @@
+
+Goal was to control Digital Pot according to some condition on the voltage measured
+Condition was to break once a certain threshold of voltage was reached
+
+Voltage change was observed with a multimeter and was visually verified by a simple LED.
+
+## Circuit Diagram:
+
+
