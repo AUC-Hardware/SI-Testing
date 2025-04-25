d96b84ce65894e9bb4753ae54c3d7fe10a454606
Waseem <ahmedwaseemr@aucegypt.edu>
2025-04-25 23:10:51 +0300

    Working PID and First Test Data

--- Experiments/PIDController/PIDController.py
+++ Experiments/PIDController/PIDController.py
@@ -42,7 +42,7 @@ class TempReader:
                     print("Temperature= {}".format(self._ds.read_temp(device)))
             else:
                 print("ERROR: No Devices to read from")
-            await uasyncio.sleep_ms(250)
+            await uasyncio.sleep_ms(400)
 
     async def update_temp_readings_loop(self):
         while(self._startTempRead):
@@ -51,7 +51,7 @@ class TempReader:
                 self.currentReadTemp = self._ds.read_temp(self._devices[0])
             else:
                 print("ERROR: No Devices to read from")
-            await uasyncio.sleep_ms(250)
+            await uasyncio.sleep_ms(400)
 
 
 # some of the code Ported and modified from Phil's Lab C implementation of PID controller
@@ -62,6 +62,7 @@ class PID:
 
     _startPID = False
     _pid_pin = Pin(PID_PIN, Pin.OUT)
+    _duty : int = 0
     ##
 
     def __init__(
@@ -102,7 +103,7 @@ class PID:
         proportional: float = self.Kp * error
 
         # intergral component #
-        self._integrator = self._integrator + 0.5 * self._Ki * self._T * (error + self._prev_error)
+        self._integrator = self._integrator + 0.5 * self.Ki * self._T * (error + self._prev_error)
 
         # Integrator anti windup #
         # Since integral component can go over other channels
@@ -126,7 +127,8 @@ class PID:
             self._integrator = limMinF
 
         # Derivative band limited differentiation
-        self._differentiator = (2.0 * self.Kd * (measurement - self._prev_measurement)) + (2.0 * self._tau - self._T * self._differentiator) / (2.0 * self._tau + self._T)
+        self._differentiator = (2.0 * self.Kd * (measurement - self._prev_measurement) + (2.0 * self._tau - self._T) * self._differentiator)
+        self._differentiator /= (2.0 * self._tau + self._T)
 
         self.out = proportional + self._integrator + self._differentiator
 
@@ -134,38 +136,51 @@ class PID:
         self._prev_measurement = measurement
 
     # i in range 0 to 65025 signyfing duty cycle %
-    async def pwm_controller_loop(self, i):
+    async def pwm_controller_loop(self):
         while (self._startPID):
             self._pid_pin.on()
-            await uasyncio.sleep((self.period)*(i/INT16_MAX))
+            await uasyncio.sleep((self.period)*(self._duty/100))
             self._pid_pin.off()
-            await uasyncio.sleep((self.period)*(1- i/INT16_MAX))
+            await uasyncio.sleep((self.period)*(1 - self._duty/100))
 
     def startPID(self):
         self._startPID = True
 
     def stopPID(self):
         self._startPID = False
+    
+    def set_duty(self, duty):
+        self._duty = duty
+    
 
 async def main():
-    pidController = PID(1, 0 ,0, 0, 0, 100, 250, 0, 0 ,0 ,0, 23)
-    pidController.period = 1
+    pidController = PID(1, 2, 1,
+                        1, -50, 50, 0.450, 0, 0 ,0 ,0, 23)
+    pidController.period = 2
     pidController.startPID()
     tempReader = TempReader()
 
     if (tempReader.scan_devices()):
         tempReader.startTempRead()
-        uasyncio.create_task(pidController.pwm_controller_loop(INT16_MAX/2))
+        uasyncio.create_task(pidController.pwm_controller_loop())
         uasyncio.create_task(tempReader.update_temp_readings_loop())
         uasyncio.create_task(tempReader.print_temp_readings_loop())
     else:
         return
+    csv = open("data.csv", "at")
+    time: int = 0
 
     while True:
         if (tempReader.currentReadTemp is not None):
             pidController.pid_update(40, tempReader.currentReadTemp)
-        print(pidController.out)
+            if (pidController.out > 0):
+                pidController.set_duty(pidController.out % 100)
+            else:
+                pidController.set_duty(0)
+            print(pidController.out)
+            csv.write("{}, {}, {}\n".format(pidController.out, tempReader.currentReadTemp, time))
+        time += 1
         await uasyncio.sleep_ms(40)
 
 # run our entry point main()
-uasyncio.run(main())
+uasyncio.run(uasyncio.gather(main()))
